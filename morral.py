7488e1b98219fc072067a11c62796ceb9b22ac91
Mario <marioriascos1201@gmail.com>
2022-11-20 15:13:06 -0500

    última clase

---
+++ morral.py
@@ -0,0 +1,15 @@
+
+def backpack(size_backpack, weights, values, n):
+    
+    pass
+
+if __name__ == '__main__':
+    
+    values = [60,100,120]
+    weights = [10,20,30]
+    size_backpack = 50
+    n = len(values)
+
+    res = backpack(size_backpack, weights, values, n)
+    print(res)
+
